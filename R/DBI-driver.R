8af8be3cdfa030e99dbbc8c32dadcc751df86863
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2017-06-23 16:59:16 +0100

    Get rid of dbConnect and dbDisconnect

--- R/DBI-driver.R
+++ R/DBI-driver.R
@@ -1,22 +1,2 @@
 #' @include DBI.R
 NULL
-
-#' Fetch a connection from the pool
-#'
-#' Fetch a DBIConnection object from the pool (a wrapper around
-#' \code{poolCheckout(pool)}). This method should be used with
-#' care, since this puts the onus of releasing the connection
-#' on you (the user). While this means that you still enjoy the
-#' performance benefits associated with having a pool, you lose
-#' the benefits of automatic connection management, since you
-#' are now yourself responsible for releasing the connection
-#' (using \code{poolReturn(conn)} or \code{dbDisconnect(conn)})
-#' at the appropriate time.
-#'
-#' @param drv The pool object to fetch the connection from.
-#' @param ... Not in use.
-#'
-#' @export
-setMethod("dbConnect", "Pool", function(drv, ...) {
-  poolCheckout(drv)
-})
--- R/DBI-pool.R
+++ R/DBI-pool.R
@@ -1,22 +1,6 @@
 #' @include DBI.R
 NULL
 
-#' Release a connection back to the pool
-#'
-#' A wrapper around \code{poolReturn(conn)}. Unlike its namesake in DBI,
-#' in the context of the \code{pool} package, \code{dbDisconnect} does
-#' not close the connection and free the associated resources. Rather,
-#' it simply returns the connection back to pool, which then decides
-#' whether to keep it around or actually destroy it.
-#'
-#' @param conn A DBIConnection object previously fetched from the pool.
-#' @param ... Not in use.
-#'
-#' @export
-setMethod("dbDisconnect", "DBIConnection", function(conn, ...) {
-  poolReturn(conn)
-})
-
 ## This method used to attempt to clean up for you if it
 ## detected open results sets. However, different database
 ## drivers implement different DBI methods and this quickly
