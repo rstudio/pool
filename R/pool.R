eadfeb08cd04cd034c77386eaedbe440bb993cea
caewok <caewok_2000@yahoo.com>
2017-10-26 12:15:39 -0700

    Fix for determining interval for validating objects (#48)
    Fix for determining interval for validating objects. Sys.time - lastValidated can result in minutes or other units; use difftime to force the result to be in seconds.

--- R/pool.R
+++ R/pool.R
@@ -309,7 +309,9 @@ Pool <- R6::R6Class("Pool",
       if (is.null(lastValidated)) {
         lastValidated <- Sys.time() - self$validationInterval
       }
-      interval <- Sys.time() - lastValidated
+
+      interval <- difftime(Sys.time(), lastValidated, units = "secs") # ensure the interval is returned in seconds
+
       if (interval >= self$validationInterval) {
         onValidate(object)
         pool_metadata$lastValidated <- Sys.time()
