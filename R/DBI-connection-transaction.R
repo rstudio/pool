4205feb8a685384fb8db93114c35220fb9611752
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2017-11-03 16:02:22 -0400

    Use `requireNamespace("pkg", quietly = TRUE)` for RMySQL and RSQLite in the examples/tests since these are "Suggests" packages (i.e. not "Depends")

--- R/DBI-connection-transaction.R
+++ R/DBI-connection-transaction.R
@@ -106,40 +106,45 @@ setMethod("dbWithTransaction", "Pool", function(conn, code) {
 #' @return \code{func}'s return value.
 #' @export
 #' @examples
-#' pool <- dbPool(RSQLite::SQLite(), dbname = ":memory:")
+#' if (requireNamespace("RSQLite", quietly = TRUE)) {
+#'   pool <- dbPool(RSQLite::SQLite(), dbname = ":memory:")
 #'
-#' dbWriteTable(pool, "cars", head(cars, 3))
-#' dbReadTable(pool, "cars")   # there are 3 rows
+#'   dbWriteTable(pool, "cars", head(cars, 3))
+#'   dbReadTable(pool, "cars")   # there are 3 rows
 #'
-#' ## successful transaction
-#' poolWithTransaction(pool, function(conn) {
-#'   dbExecute(conn, "INSERT INTO cars (speed, dist) VALUES (1, 1);")
-#'   dbExecute(conn, "INSERT INTO cars (speed, dist) VALUES (2, 2);")
-#'   dbExecute(conn, "INSERT INTO cars (speed, dist) VALUES (3, 3);")
-#' })
-#' dbReadTable(pool, "cars")   # there are now 6 rows
-#'
-#' ## failed transaction -- note the missing comma
-#' tryCatch(
+#'   ## successful transaction
 #'   poolWithTransaction(pool, function(conn) {
 #'     dbExecute(conn, "INSERT INTO cars (speed, dist) VALUES (1, 1);")
-#'     dbExecute(conn, "INSERT INTO cars (speed dist) VALUES (2, 2);")
+#'     dbExecute(conn, "INSERT INTO cars (speed, dist) VALUES (2, 2);")
 #'     dbExecute(conn, "INSERT INTO cars (speed, dist) VALUES (3, 3);")
-#'   }),
-#'   error = identity
-#' )
-#' dbReadTable(pool, "cars")   # still 6 rows
+#'   })
+#'   dbReadTable(pool, "cars")   # there are now 6 rows
 #'
-#' ## early exit, silently
-#' poolWithTransaction(pool, function(conn) {
-#'   dbExecute(conn, "INSERT INTO cars (speed, dist) VALUES (1, 1);")
-#'   dbExecute(conn, "INSERT INTO cars (speed, dist) VALUES (2, 2);")
-#'   if (nrow(dbReadTable(conn, "cars")) > 7) dbBreak()
-#'   dbExecute(conn, "INSERT INTO cars (speed, dist) VALUES (3, 3);")
-#' })
-#' dbReadTable(pool, "cars")   # still 6 rows
+#'   ## failed transaction -- note the missing comma
+#'   tryCatch(
+#'     poolWithTransaction(pool, function(conn) {
+#'       dbExecute(conn, "INSERT INTO cars (speed, dist) VALUES (1, 1);")
+#'       dbExecute(conn, "INSERT INTO cars (speed dist) VALUES (2, 2);")
+#'       dbExecute(conn, "INSERT INTO cars (speed, dist) VALUES (3, 3);")
+#'     }),
+#'     error = identity
+#'   )
+#'   dbReadTable(pool, "cars")   # still 6 rows
 #'
-#' poolClose(pool)
+#'   ## early exit, silently
+#'   poolWithTransaction(pool, function(conn) {
+#'     dbExecute(conn, "INSERT INTO cars (speed, dist) VALUES (1, 1);")
+#'     dbExecute(conn, "INSERT INTO cars (speed, dist) VALUES (2, 2);")
+#'     if (nrow(dbReadTable(conn, "cars")) > 7) dbBreak()
+#'     dbExecute(conn, "INSERT INTO cars (speed, dist) VALUES (3, 3);")
+#'   })
+#'   dbReadTable(pool, "cars")   # still 6 rows
+#'
+#'   poolClose(pool)
+#'
+#' } else {
+#'   message("Please install the 'RSQLite' package to run this example")
+#' }
 poolWithTransaction <- function(pool, func) {
   conn <- poolCheckout(pool)
   on.exit(poolReturn(conn))
--- R/DBI.R
+++ R/DBI.R
@@ -38,23 +38,28 @@ NULL
 #'
 #' @export
 #' @examples
-#' pool <- dbPool(
-#'   drv = RMySQL::MySQL(),
-#'   dbname = "shinydemo",
-#'   host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
-#'   username = "guest",
-#'   password = "guest"
-#' )
+#' if (requireNamespace("RMySQL", quietly = TRUE)) {
+#'   pool <- dbPool(
+#'     drv = RMySQL::MySQL(),
+#'     dbname = "shinydemo",
+#'     host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
+#'     username = "guest",
+#'     password = "guest"
+#'   )
 #'
-#' dbGetQuery(pool, "SELECT * from City LIMIT 5;")
-#' #>   ID           Name CountryCode      District Population
-#' #> 1  1          Kabul         AFG         Kabol    1780000
-#' #> 2  2       Qandahar         AFG      Qandahar     237500
-#' #> 3  3          Herat         AFG         Herat     186800
-#' #> 4  4 Mazar-e-Sharif         AFG         Balkh     127800
-#' #> 5  5      Amsterdam         NLD Noord-Holland     731200
+#'   dbGetQuery(pool, "SELECT * from City LIMIT 5;")
+#'   #>   ID           Name CountryCode      District Population
+#'   #> 1  1          Kabul         AFG         Kabol    1780000
+#'   #> 2  2       Qandahar         AFG      Qandahar     237500
+#'   #> 3  3          Herat         AFG         Herat     186800
+#'   #> 4  4 Mazar-e-Sharif         AFG         Balkh     127800
+#'   #> 5  5      Amsterdam         NLD Noord-Holland     731200
 #'
-#' poolClose(pool)
+#'   poolClose(pool)
+#'
+#' } else {
+#'   message("Please install the 'RMySQL' package to run this example")
+#' }
 dbPool <- function(drv, ..., validateQuery = NULL) {
   state <- new.env(parent = emptyenv())
   state$validateQuery <- validateQuery
