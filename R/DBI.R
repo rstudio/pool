e5150d6b681c61047402122b72be474f6c0bfd3e
Hadley Wickham <h.wickham@gmail.com>
2020-09-24 13:42:20 -0500

    Tidy up docs and description (#104)
    * Use latest roxygen2
    * Generate all docs with roxygen2
    * Update package doc to modern style
    * Use markdown in roxygen
    * Improve documentation of S4 methods
    * use_tidy_description()

--- R/DBI.R
+++ R/DBI.R
@@ -3,37 +3,37 @@ NULL
 
 #' Create a DBI Database Connection Pool.
 #'
-#' A wrapper around \code{poolCreate} to simplify the creation of a DBI
-#' database connection pool. Check the documentation of \code{\link{poolCreate}}
+#' A wrapper around `poolCreate` to simplify the creation of a DBI
+#' database connection pool. Check the documentation of [poolCreate()]
 #' for a generic overview of the parent function and the Pool object. The
-#' main thing to point out is that, for \code{dbPool}, you always need to
-#' provide a DBI driver (i.e. of class \code{\link[DBI]{DBIDriver-class}}),
+#' main thing to point out is that, for `dbPool`, you always need to
+#' provide a DBI driver (i.e. of class [DBI::DBIDriver-class()]),
 #' and it should always be accompanied by the required authorization
 #' arguments (see the example below).
 #'
-#' @param drv An object that inherits from \code{\link[DBI]{DBIDriver-class}},
-#'   or an existing \code{\link[DBI]{DBIConnection-class}} object (in order to
+#' @param drv An object that inherits from [DBI::DBIDriver-class()],
+#'   or an existing [DBI::DBIConnection-class()] object (in order to
 #'   clone an existing connection).
 #'
-#' @param ... Arguments needed for both \code{\link[DBI]{dbConnect}}
-#' (mandatory if required by the \code{DBIDriver} you're using) and
-#' \code{\link{poolCreate}} (optional) - all arguments should be named:
+#' @param ... Arguments needed for both [DBI::dbConnect()]
+#' (mandatory if required by the `DBIDriver` you're using) and
+#' [poolCreate()] (optional) - all arguments should be named:
 #' \enumerate{
 #'   \item The required authorization arguments needed by the DBMS
-#'   instance; these typically include \code{user}, \code{password},
-#'   \code{dbname}, \code{host}, \code{port}, etc. For details check
-#'   the appropriate \code{DBIDriver}'s documentation.
-#'   \item Optionally, override the \code{poolCreate} defaults:
-#'   \code{minSize} (minimum number of connections that the pool should
-#'   have at all times), \code{maxSize} (maximum number of connections
-#'   that the pool may have at any time), \code{idleTimeout} (number
+#'   instance; these typically include `user`, `password`,
+#'   `dbname`, `host`, `port`, etc. For details check
+#'   the appropriate `DBIDriver`'s documentation.
+#'   \item Optionally, override the `poolCreate` defaults:
+#'   `minSize` (minimum number of connections that the pool should
+#'   have at all times), `maxSize` (maximum number of connections
+#'   that the pool may have at any time), `idleTimeout` (number
 #'   of seconds to wait before closing a connection, if the number
-#'   of connection is above \code{minSize}), and \code{validationInterval}
+#'   of connection is above `minSize`), and `validationInterval`
 #'   (number of seconds to wait before validating the connection again).
 #' }
 #' @param validateQuery The query to run to verify that the connection
 #' is valid (it should be as simple as possible). If this is not
-#' provided, \code{dbPool} will try a few possibilities, but these are
+#' provided, `dbPool` will try a few possibilities, but these are
 #' not exhaustive.
 #'
 #' @export
