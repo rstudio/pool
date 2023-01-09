78bb1cf11d51ca3027b14530497f600111bfa18f
Hadley Wickham <h.wickham@gmail.com>
2023-01-09 14:02:39 -0600

    Wrap all DBI methods the same way

---
+++ R/DBI-custom.R
@@ -0,0 +1,139 @@
+#' @include DBI.R
+NULL
+
+#' DBI methods
+#'
+#' @description
+#' Pool methods for DBI generics generally check out a connection
+#' (with [poolCheckout()]), perform the operation, and the return the connection
+#' to the pool (with [poolReturn()]). This page describes the exceptions.
+#'
+#' Pool cannot implement the [DBI::dbSendQuery()] and [DBI::dbSendStatement()]
+#' methods because they both return result sets that are inextricably bound
+#' to a specific connection, and there's no guarantee that connection will
+#' still be alive once you retrieve the results. Instead use [DBI::dbGetQuery()]
+#' and [DBI::dbExecute()].
+#'
+#' Similarly, pool cannot implement the [DBI::dbBegin()], [DBI::dbRollback()],
+#' or [DBI::dbCommit()] generics because there's no guarantee that you'll get
+#' the same connection from call to call. Instead use [poolWithTransaction()].
+#'
+#' @param conn,dbObj A Pool object, as returned from [dbPool()].
+#' @param statement,code,... See DBI documentation.
+#' @name DBI-custom
+#' @examples
+#' if (requireNamespace("RSQLite", quietly = TRUE)) {
+#'   mtcars1 <- mtcars[ c(1:16), ] # first half of the mtcars dataset
+#'   mtcars2 <- mtcars[-c(1:16), ] # second half of the mtcars dataset
+#'
+#'   pool <- dbPool(RSQLite::SQLite(), dbname = ":memory:")
+#'
+#'   # write the mtcars1 table into the database
+#'   dbWriteTable(pool, "mtcars", mtcars1, row.names = TRUE)
+#'
+#'   # list the current tables in the database
+#'   dbListTables(pool)
+#'
+#'   # read the "mtcars" table from the database (only 16 rows)
+#'   dbReadTable(pool, "mtcars")
+#'
+#'   # append mtcars2 to the "mtcars" table already in the database
+#'   dbWriteTable(pool, "mtcars", mtcars2, row.names = TRUE, append = TRUE)
+#'
+#'   # read the "mtcars" table from the database (all 32 rows)
+#'   dbReadTable(pool, "mtcars")
+#'
+#'   # get the names of the columns in the databases's table
+#'   dbListFields(pool, "mtcars")
+#'
+#'   # use dbExecute to change the "mpg" and "cyl" values of the 1st row
+#'   dbExecute(pool,
+#'     paste(
+#'       "UPDATE mtcars",
+#'       "SET mpg = '22.0', cyl = '10'",
+#'       "WHERE row_names = 'Mazda RX4'"
+#'     )
+#'   )
+#'
+#'   # read the 1st row of "mtcars" table to confirm the previous change
+#'   dbGetQuery(pool, "SELECT * FROM mtcars WHERE row_names = 'Mazda RX4'")
+#'
+#'   # drop the "mtcars" table from the database
+#'   dbRemoveTable(pool, "mtcars")
+#'
+#'   # list the current tables in the database
+#'   dbListTables(pool)
+#'
+#'   poolClose(pool)
+#'
+#' } else {
+#'   message("Please install the 'RSQLite' package to run this example")
+#' }
+NULL
+
+## Throw error here since this would require keeping a connection
+## open and never releasing it back to the pool.
+#' @export
+#' @rdname DBI-custom
+setMethod("dbSendQuery", "Pool", function(conn, statement, ...) {
+  stop("Pool does not implement the dbSendQuery() method. Either use dbGetQuery(), or `conn <- poolCheckout(pool); dbSendQuery(conn, ...)` (and call `poolReturn(conn)` when finished).")
+})
+
+#' @export
+#' @rdname DBI-custom
+setMethod("dbSendStatement", "Pool", function(conn, statement, ...) {
+  stop("Pool does not implement the dbSendStatement() method. Use dbExecute() instead.")
+})
+
+#' @export
+#' @rdname DBI-custom
+setMethod("dbGetInfo", "Pool", function(dbObj, ...) {
+  pooledObj <- poolCheckout(dbObj)
+  on.exit(poolReturn(pooledObj))
+  list(class = is(dbObj),
+       valid = dbObj$valid,
+       minSize = dbObj$minSize,
+       maxSize = dbObj$maxSize,
+       idleTimeout = dbObj$idleTimeout,
+       pooledObjectClass = is(pooledObj)[1],
+       numberFreeObjects = dbObj$counters$free,
+       numberTakenObjects = dbObj$counters$taken)
+})
+
+
+#' @export
+#' @rdname DBI-custom
+setMethod("dbBegin", "Pool", function(conn, ...) {
+  stop("Must use `conn <- poolCheckout(pool); dbBegin(conn, ...)` ",
+       "instead. Remember to `poolReturn(conn)` when `conn` is ",
+       "no longer necessary. Consider using `poolWithTransaction(...)` ",
+       "for simple transactions.")
+})
+
+#' @export
+#' @rdname DBI-custom
+setMethod("dbCommit", "Pool", function(conn, ...) {
+  stop("Must use `conn <- poolCheckout(pool); dbCommit(conn, ...)` ",
+       "instead. Remember to `poolReturn(conn)` when `conn` is ",
+       "no longer necessary. Consider using `poolWithTransaction(...)` ",
+       "for simple transactions.")
+})
+
+#' @export
+#' @rdname DBI-custom
+setMethod("dbRollback", "Pool", function(conn, ...) {
+  stop("Must use `conn <- poolCheckout(pool); dbRollback(conn, ...)` ",
+       "instead. Remember to `poolReturn(conn)` when `conn` is ",
+       "no longer necessary. Consider using `poolWithTransaction(...)` ",
+       "for simple transactions.")
+})
+
+
+#' @export
+#' @rdname DBI-custom
+setMethod("dbWithTransaction", "Pool", function(conn, code) {
+  stop("Must use `conn <- poolCheckout(pool); dbWithTransaction(conn, ...)` ",
+       "instead. Remember to `poolReturn(conn)` when `conn` is ",
+       "no longer necessary. Consider using `poolWithTransaction(...)` ",
+       "for simple transactions.")
+})
---
+++ R/DBI-wrap.R
@@ -0,0 +1,135 @@
+#' Custom DBI methods for Pool
+#'
+#' @description
+#' Wrappers around all DBI methods that check out a connection
+#' (with [poolCheckout()]), perform the operation, and the return the connection
+#' to the pool (with [poolReturn()]).
+#'
+#' @name DBI-wrap
+#' @keywords internal
+NULL
+
+# Wrapped methods ---------------------------------------------------------
+
+DBI_wrap <- function(fun_name) {
+  fun <- utils::getFromNamespace(fun_name, "DBI")
+
+  args <- formals(fun)
+  con_arg <- sym(names(args)[[1]])
+
+  call_args <- syms(set_names(names(args)))
+  call_args[[1]] <- quote(db_con)
+  recall <- call2(call2("::", quote(DBI), sym(fun_name)), !!!call_args)
+
+  con <- NULL # quiet R CMD check note
+  body <- expr({
+    db_con <- poolCheckout(!!con_arg)
+    on.exit(poolReturn(db_con))
+
+    !!recall
+  })
+
+  new_function(args, body, env = ns_env("pool"))
+}
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("dbGetQuery", "Pool", DBI_wrap("dbGetQuery"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("dbExecute", "Pool", DBI_wrap("dbExecute"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("dbListResults", "Pool", function(conn, ...) {
+  list()
+})
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("dbListFields", "Pool", DBI_wrap("dbListFields"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("dbListTables", "Pool", DBI_wrap("dbListTables"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("dbListObjects", "Pool", DBI_wrap("dbListObjects"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("dbReadTable", "Pool", DBI_wrap("dbReadTable"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("dbWriteTable", "Pool", DBI_wrap("dbWriteTable"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("dbCreateTable", "Pool", DBI_wrap("dbCreateTable"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("dbAppendTable", "Pool", DBI_wrap("dbAppendTable"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("dbExistsTable", "Pool", DBI_wrap("dbExistsTable"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("dbRemoveTable", "Pool", DBI_wrap("dbRemoveTable"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("dbIsReadOnly", "Pool", DBI_wrap("dbIsReadOnly"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("make.db.names", "Pool", DBI_wrap("make.db.names"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("isSQLKeyword", "Pool", DBI_wrap("isSQLKeyword"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("SQLKeywords", "Pool", DBI_wrap("SQLKeywords"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("sqlData", "Pool", DBI_wrap("sqlData"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("sqlCreateTable", "Pool", DBI_wrap("sqlCreateTable"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("sqlAppendTable", "Pool", DBI_wrap("sqlAppendTable"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("sqlInterpolate", "Pool", DBI_wrap("sqlInterpolate"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("sqlParseVariables", "Pool", DBI_wrap("sqlParseVariables"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("dbQuoteIdentifier", "Pool", DBI_wrap("dbQuoteIdentifier"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("dbUnquoteIdentifier", "Pool", DBI_wrap("dbUnquoteIdentifier"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("dbQuoteLiteral", "Pool", DBI_wrap("dbQuoteLiteral"))
+
+#' @export
+#' @rdname DBI-wrap
+setMethod("dbQuoteString", "Pool", DBI_wrap("dbQuoteString"))
