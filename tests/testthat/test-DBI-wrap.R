c1405fe669b66915f790a385909477265bd3d905
Hadley Wickham <h.wickham@gmail.com>
2023-01-14 11:31:24 -0600

    Skip test on coverage

--- tests/testthat/test-DBI-wrap.R
+++ tests/testthat/test-DBI-wrap.R
@@ -1,4 +1,8 @@
 test_that("wrapper functions look good", {
+  # Skip in test coverage runs because covr instrumentation is injected
+  # into function body
+  skip_if_not(is.null(getOption("covr.flags")))
+
   expect_snapshot({
     DBI_wrap("dbExecute")
   })
