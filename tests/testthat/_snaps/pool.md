d63ff898bb294584e53e078da876dde2014b71ab
Hadley Wickham <h.wickham@gmail.com>
2023-02-07 17:40:56 -0600

    Refactor basic pool tests

--- tests/testthat/_snaps/pool.md
+++ tests/testthat/_snaps/pool.md
@@ -1,11 +1,3 @@
-# object operations: enforces maxSize
-
-    Code
-      poolCheckout(pool)
-    Condition
-      Error in `poolCheckout()`:
-      ! Maximum number of objects in pool has been reached
-
 # pool has useful print method
 
     Code
--- tests/testthat/test-pool.R
+++ tests/testthat/test-pool.R
@@ -1,63 +1,23 @@
-describe("pool", {
-  local_reproducible_output()
+test_that("can be created and closed", {
+  pool <- poolCreate(function() 1)
+  checkCounts(pool, free = 1, taken = 0)
+  expect_true(pool$valid)
 
-  describe("basic mechanics", {
-    pool <- poolCreate(MockPooledObj$new,
-      minSize = 1, maxSize = 3, idleTimeout = 1)
+  poolClose(pool)
+  checkCounts(pool, free = 0, taken = 0)
+  expect_false(pool$valid)
+})
 
-    it("can be created", {
-      expect_s3_class(pool, "Pool")
-      expect_s3_class(pool, "R6")
-    })
+test_that("it can fetch and release", {
+  pool <- poolCreate(function() 1)
+  withr::defer(poolClose(pool))
 
-    it("respects validity", {
-      expect_true(pool$valid)
-      poolClose(pool)
-      expect_false(pool$valid)
-    })
+  obj <- poolCheckout(pool)
+  expect_equal(obj, 1, ignore_attr = TRUE)
+  checkCounts(pool, free = 0, taken = 1)
 
-    it("destroys all free objects when closed", {
-      checkCounts(pool, free = 0, taken = 0)
-    })
-
-    # it("finalizer runs", {
-    #   pool <- poolCreate(MockPooledObj$new,
-    #     minSize = 1, maxSize = 3, idleTimeout = 1)
-    #   rm(pool)
-    # })
-  })
-
-  describe("object operations", {
-    pool <- poolCreate(MockPooledObj$new,
-      minSize = 1, maxSize = 3, idleTimeout = 1)
-
-    it("supports generic fetch/release", {
-      checkCounts(pool, free = 1, taken = 0)
-
-      obj <- poolCheckout(pool)
-      checkCounts(pool, free = 0, taken = 1)
-      expect_s3_class(obj, "MockPooledObj")
-
-      poolReturn(obj)
-      checkCounts(pool, free = 1, taken = 0)
-      expect_false(obj$closed)
-    })
-
-    it("enforces maxSize", {
-      pool <- poolCreate(function() 1, maxSize = 3)
-
-      a <- poolCheckout(pool)
-      b <- poolCheckout(pool)
-      c <- poolCheckout(pool)
-      expect_snapshot(poolCheckout(pool), error = TRUE)
-      objs <- list(a, b, c)
-      lapply(objs, poolReturn)
-
-      poolClose(pool)
-    })
-
-    poolClose(pool)
-  })
+  poolReturn(obj)
+  checkCounts(pool, free = 1, taken = 0)
 })
 
 test_that("pool has useful print method", {
