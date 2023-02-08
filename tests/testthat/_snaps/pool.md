03d0258ba07093d1dc5b3b94d6d626be8b564de0
Hadley Wickham <h.wickham@gmail.com>
2023-02-08 11:10:05 -0600

    Refactor tests (#160)
    * Use a helper instead of `utils.R`
    * Eliminate `describe()` in favour of `test_that()`, and keep each test independent
    * Move all pool tests into `test-pool.R`

--- tests/testthat/_snaps/pool.md
+++ tests/testthat/_snaps/pool.md
@@ -1,10 +1,93 @@
-# object operations: enforces maxSize
+# it requires a valid factory
+
+    Code
+      poolCreate(1)
+    Condition
+      Error in `poolCreate()`:
+      ! `factory` must be a function.
+    Code
+      poolCreate(function(x) NULL)
+    Condition
+      Error in `poolCreate()`:
+      ! Object creation failed.
+      * The `factory` must not return `NULL`
+
+# pool can't be closed twice
+
+    Code
+      poolCheckout(pool)
+    Condition
+      Error in `poolCheckout()`:
+      ! This pool is no longer valid. Cannot fetch new objects.
+
+# max size is enforced
 
     Code
       poolCheckout(pool)
     Condition
       Error in `poolCheckout()`:
       ! Maximum number of objects in pool has been reached
+
+# warns if validation fails once, creates new object and tries again
+
+    Code
+      obj <- check_valid_object(badObject)
+    Output
+      <pool> Failed to activate and/or validate existing object.
+      <pool> Trying again with a new object.
+
+---
+
+    Code
+      check_valid_object(obj)
+    Output
+      <pool> Failed to activate and/or validate existing object.
+      <pool> Trying again with a new object.
+    Condition
+      Error in `check_valid_object()`:
+      ! Freshly created object does not appear to be valid.
+      Caused by error in `onValidate()`:
+      ! Validation failed...
+
+# can't return the same object twice
+
+    Code
+      poolReturn(obj)
+    Condition
+      Error in `poolReturn()`:
+      ! This object was already returned to the pool.
+
+# poolClose() warns about taken objects, but they can still be returned
+
+    Code
+      poolClose(pool)
+    Output
+      <pool> You still have checked out objects.
+      <pool> Use `poolReturn()` them to the pool so they can be destroyed.
+
+# warns if object can't be returned
+
+    Code
+      pool <- poolCreate(function() 1)
+      obj <- poolCheckout(pool)
+      rm(obj)
+      . <- gc()
+    Output
+      <pool> Checked-out object deleted before being returned.
+      <pool> Make sure to `poolReturn()` all objects retrieved with `poolCheckout().`
+    Code
+      poolClose(pool)
+    Output
+      <pool> You still have checked out objects.
+      <pool> Use `poolReturn()` them to the pool so they can be destroyed.
+
+# poolReturn() errors if object is not valid
+
+    Code
+      poolReturn("x")
+    Condition
+      Error in `poolReturn()`:
+      ! `object` is not an pooled object.
 
 # pool has useful print method
 
@@ -48,4 +131,51 @@
         Available in pool: 0
         Max size: Inf
         Valid: TRUE
+
+# useful warning if onDestroy fails
+
+    Code
+      poolReturn(b)
+      later::run_now()
+    Output
+      <pool> Object could not be destroyed, but was removed from the pool.
+      <pool> Error message:
+      <pool>   Destruction failed...
+
+# throws if onPassivate fails
+
+    Code
+      poolReturn(obj)
+    Condition
+      Error in `poolReturn()`:
+      ! Object could not be returned back to the pool.
+      * It was destroyed instead
+      Caused by error in `onPassivate()`:
+      ! Passivation failed...
+
+# throws if onActivate fails
+
+    Code
+      poolCheckout(pool)
+    Output
+      <pool> Failed to activate and/or validate existing object.
+      <pool> Trying again with a new object.
+    Condition
+      Error in `poolCheckout()`:
+      ! Freshly created object does not appear to be valid.
+      Caused by error in `onActivate()`:
+      ! Activation failed...
+
+# throws if onValidate fails
+
+    Code
+      poolCheckout(pool)
+    Output
+      <pool> Failed to activate and/or validate existing object.
+      <pool> Trying again with a new object.
+    Condition
+      Error in `poolCheckout()`:
+      ! Freshly created object does not appear to be valid.
+      Caused by error in `onValidate()`:
+      ! Validation failed...
 
--- tests/testthat/test-pool.R
+++ tests/testthat/test-pool.R
@@ -1,70 +1,158 @@
-source("utils.R")
+test_that("can be created and closed", {
+  pool <- poolCreate(function() 1)
+  checkCounts(pool, free = 1, taken = 0)
+  expect_true(pool$valid)
 
-describe("pool", {
-  local_reproducible_output()
+  poolClose(pool)
+  checkCounts(pool, free = 0, taken = 0)
+  expect_false(pool$valid)
+})
 
-  describe("basic mechanics", {
-    pool <- poolCreate(MockPooledObj$new,
-      minSize = 1, maxSize = 3, idleTimeout = 1)
-
-    it("can be created", {
-      expect_s3_class(pool, "Pool")
-      expect_s3_class(pool, "R6")
-    })
-
-    it("respects validity", {
-      expect_true(pool$valid)
-      poolClose(pool)
-      expect_false(pool$valid)
-    })
-
-    it("destroys all free objects when closed", {
-      checkCounts(pool, free = 0, taken = 0)
-    })
-
-    # it("finalizer runs", {
-    #   pool <- poolCreate(MockPooledObj$new,
-    #     minSize = 1, maxSize = 3, idleTimeout = 1)
-    #   rm(pool)
-    # })
+test_that("it requires a valid factory", {
+  expect_snapshot(error = TRUE, {
+    poolCreate(1)
+    poolCreate(function(x) NULL)
   })
+})
 
-  describe("object operations", {
-    pool <- poolCreate(MockPooledObj$new,
-      minSize = 1, maxSize = 3, idleTimeout = 1)
+test_that("pool can't be closed twice", {
+  pool <- poolCreate(function() 1)
+  poolClose(pool)
 
-    it("supports generic fetch/release", {
-      checkCounts(pool, free = 1, taken = 0)
+  expect_snapshot(poolCheckout(pool), error = TRUE)
+})
 
-      obj <- poolCheckout(pool)
-      checkCounts(pool, free = 0, taken = 1)
-      expect_s3_class(obj, "MockPooledObj")
+test_that("can fetch and release", {
+  pool <- poolCreate(function() 1)
+  withr::defer(poolClose(pool))
 
-      poolReturn(obj)
-      checkCounts(pool, free = 1, taken = 0)
-      expect_false(obj$closed)
-    })
+  obj <- poolCheckout(pool)
+  expect_equal(obj, 1, ignore_attr = TRUE)
+  checkCounts(pool, free = 0, taken = 1)
 
-    it("enforces maxSize", {
-      pool <- poolCreate(function() 1, maxSize = 3)
+  poolReturn(obj)
+  checkCounts(pool, free = 1, taken = 0)
+})
 
-      a <- poolCheckout(pool)
-      b <- poolCheckout(pool)
-      c <- poolCheckout(pool)
-      expect_snapshot(poolCheckout(pool), error = TRUE)
-      objs <- list(a, b, c)
-      lapply(objs, poolReturn)
+test_that("max size is enforced", {
+  pool <- poolCreate(MockPooledObj$new, maxSize = 2)
+  withr::defer(poolClose(pool))
 
-      poolClose(pool)
-    })
+  obj1 <- poolCheckout(pool)
+  obj2 <- poolCheckout(pool)
 
+  expect_snapshot(poolCheckout(pool), error = TRUE)
+
+  poolReturn(obj1)
+  poolReturn(obj2)
+})
+
+test_that("idle objects are reaped", {
+  pool <- poolCreate(MockPooledObj$new, idleTimeout = 0)
+  withr::defer(poolClose(pool))
+
+  obj1 <- poolCheckout(pool)
+  obj2 <- poolCheckout(pool)
+  poolReturn(obj1)
+  poolReturn(obj2)
+
+  checkCounts(pool, free = 2, taken = 0)
+  later::run_now() # force scheduler to run NOW
+  checkCounts(pool, free = 1, taken = 0)
+})
+
+test_that("validates (only) when needed", {
+  pool <- poolCreate(MockPooledObj$new, validationInterval = 0.1)
+  withr::defer(poolClose(pool))
+
+  last_validated <- function(pool) {
+    obj <- localCheckout(pool)
+    pool_metadata(obj)$lastValidated
+  }
+
+  # Capture initial validation time
+  last_validated_0 <- last_validated(pool)
+
+  # After waiting less than validationInterval, validation time shouldn't change
+  Sys.sleep(pool$validationInterval / 2)
+  last_validated_1 <- last_validated(pool)
+  expect_equal(last_validated_0, last_validated_1)
+
+  # After waiting more than validationInterval, validation time should change
+  Sys.sleep(pool$validationInterval)
+  last_validated_2 <- last_validated(pool)
+  expect_lt(last_validated_0, last_validated_2)
+})
+
+test_that("warns if validation fails once, creates new object and tries again", {
+  pool <- poolCreate(MockPooledObj$new, validationInterval = 0.1)
+  withr::defer(poolClose(pool))
+
+  check_valid_object <- function(x) {
+    # Sneak into private methods
+    pool[['.__enclos_env__']]$private$checkValid(x)
+  }
+
+  # create object that will fail to validate
+  badObject <- poolCheckout(pool)
+  attr(badObject, "bad") <- TRUE
+  Sys.sleep(pool$validationInterval + .1)
+
+  # can't validate, so should create a new object
+  expect_snapshot(obj <- check_valid_object(badObject))
+
+  Sys.sleep(pool$validationInterval + .1)
+  expect_identical(obj, check_valid_object(obj))
+  # this implicitly returns badOjbect
+  checkCounts(pool, free = 1, taken = 0)
+
+  # now force all validations to fail so we get an error
+  failOnValidate <<- TRUE
+  withr::defer(failOnValidate <<- FALSE)
+
+  Sys.sleep(pool$validationInterval + .1)
+  expect_snapshot(check_valid_object(obj), error = TRUE)
+
+  # and since all objects have been destroyed the pool is empty
+  checkCounts(pool, free = 0, taken = 0)
+
+})
+
+test_that("can't return the same object twice", {
+  pool <- poolCreate(MockPooledObj$new)
+  withr::defer(poolClose(pool))
+
+  obj <- poolCheckout(pool)
+  poolReturn(obj)
+  expect_snapshot(poolReturn(obj), error = TRUE)
+})
+
+test_that("poolClose() warns about taken objects, but they can still be returned", {
+  pool <- poolCreate(MockPooledObj$new)
+
+  obj <- poolCheckout(pool)
+  expect_snapshot(poolClose(pool))
+
+  poolReturn(obj)
+})
+
+test_that("warns if object can't be returned", {
+  expect_snapshot({
+    pool <- poolCreate(function() 1)
+    obj <- poolCheckout(pool)
+    rm(obj)
+    . <- gc()
     poolClose(pool)
   })
 })
 
+test_that("poolReturn() errors if object is not valid", {
+  expect_snapshot(poolReturn("x"), error = TRUE)
+})
+
 test_that("pool has useful print method", {
   pool <- poolCreate(function() 10)
-  on.exit(poolClose(pool))
+  withr::defer(poolClose(pool))
 
   expect_snapshot({
     pool
@@ -82,9 +170,64 @@ test_that("pool has useful print method", {
 
 test_that("empty pool has useful print method", {
   pool <- poolCreate(function() 10, minSize = 0)
-  on.exit(poolClose(pool))
+  withr::defer(poolClose(pool))
 
   expect_snapshot({
     pool
   })
+})
+
+# Failure modes -----------------------------------------------------------
+
+test_that("useful warning if onDestroy fails", {
+  pool <- poolCreate(MockPooledObj$new, idleTimeout = 0)
+  withr::defer(poolClose(pool))
+
+  checkCounts(pool, free = 1, taken = 0)
+  failOnDestroy <<- TRUE
+  withr::defer(failOnDestroy <<- FALSE)
+
+  a <- poolCheckout(pool)
+  b <- poolCheckout(pool)
+
+  # since we're over minSize, returning `b` destroys it
+  expect_snapshot({
+    poolReturn(b)
+    later::run_now()
+  })
+
+  checkCounts(pool, free = 0, taken = 1)
+  poolReturn(a)
+})
+
+test_that("throws if onPassivate fails", {
+  pool <- poolCreate(MockPooledObj$new)
+  withr::defer(poolClose(pool))
+
+  obj <- poolCheckout(pool)
+  failOnPassivate <<- TRUE
+  withr::defer(failOnPassivate <<- FALSE)
+
+  expect_snapshot(poolReturn(obj), error = TRUE)
+})
+
+test_that("throws if onActivate fails", {
+  pool <- poolCreate(MockPooledObj$new)
+  withr::defer(poolClose(pool))
+
+  failOnActivate <<- TRUE
+  withr::defer(failOnActivate <<- FALSE)
+
+  expect_snapshot(poolCheckout(pool), error = TRUE)
+  checkCounts(pool, free = 0, taken = 0)
+})
+
+test_that("throws if onValidate fails", {
+  pool <- poolCreate(MockPooledObj$new)
+  withr::defer(poolClose(pool))
+
+  failOnValidate <<- TRUE
+  withr::defer(failOnValidate <<- FALSE)
+  expect_snapshot(poolCheckout(pool), error = TRUE)
+  checkCounts(pool, free = 0, taken = 0)
 })
